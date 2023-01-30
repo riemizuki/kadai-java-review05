80878338f642312746d509960708c34eed4ccb5d
riemizuki <riechan2626@gmail.com>
2023-01-30 18:43:45 +0900

    Javaプログラミング入門学習完了

---
+++ src/UserVariable.java
@@ -0,0 +1,11 @@
+
+public class UserVariable {
+
+    public static void main(String[] args) {
+        int number2 = 100;
+        String str2 = "xyz";
+        System.out.println(number2);
+        System.out.println(str2);
+    }
+
+}
