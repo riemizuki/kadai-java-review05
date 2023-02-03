41a2b82db458856c5fb64e1397389391c639d30f
riemizuki <riechan2626@gmail.com>
2023-02-03 22:55:05 +0900

    再提出

--- src/Review01.java
+++ src/Review01.java
@@ -5,10 +5,10 @@ public class Review01 {
         int price = 1500;
         int tax = 10;
         int result;
-        result = taxMethod(price, tax);
+        result = tax(price, tax);
         System.out.println(price + "円の商品の税込価格は" + (price +result) + "円(消費税は" + result + "円)です。");        
 }
-    public static int taxMethod(int price , int tax) {
+    public static int tax(int price , int tax) {
        int result = price * tax /100;
         return result;
     }
